c7f05cf46ee85ee4ec1846def72a6dd0783c4bdb
Paweł Gliwny <p.gliwny@gmail.com>
2016-10-26 14:06:44 +0200

    Add info about current wavelength and fixed plot

--- MeasureGui/MeasureGui.py
+++ MeasureGui/MeasureGui.py
@@ -22,6 +22,7 @@ class BaseMeasureInfo:
     _DEFAULT_FMT = "%(asctime)s - %(levelname)s - %(message)s"
     logger = logging.getLogger(__name__)
     OUT_MSG = ""
+    CURRENT_WAVELENGTH = ""
 
     def __init__(self):
         logging.basicConfig(
@@ -37,6 +38,7 @@ class MeasureDeviceConnect(BaseMeasureInfo):
         ldc = dev.get_ldc4005_instance()
         pm100 = dev.get_pm100_instance()
         BaseMeasureInfo.OUT_MSG += "Connections with device successful"
+        BaseMeasureInfo.CURRENT_WAVELENGTH = str(pm100.get_current_wavelength_in_nm())
     except Exception as err:
         BaseMeasureInfo.logger.error(err)
         BaseMeasureInfo.OUT_MSG += ("Error %s" % err)
@@ -46,28 +48,31 @@ class Measure:
 
     @staticmethod
     def do_measure(start_current, stop_current, numbers_of_points):
-        MeasureDeviceConnect.ldc.ld_current_in_A_setpoint(0)
-        time.sleep(1)
-        MeasureDeviceConnect.ldc.on()
-        time.sleep(3)
+        try:
+            MeasureDeviceConnect.ldc.ld_current_in_A_setpoint(0)
+            time.sleep(1)
+            MeasureDeviceConnect.ldc.on()
+            time.sleep(3)
 
-        current = list()
-        voltage = list()
-        power = list()
-        set_current_array = np.linspace(start_current, stop_current, numbers_of_points)
+            current = list()
+            voltage = list()
+            power = list()
+            set_current_array = np.linspace(start_current, stop_current, numbers_of_points)
 
-        for i in range(0, len(set_current_array)):
-            MeasureDeviceConnect.ldc.ld_current_in_A_setpoint(str(set_current_array[i]))
-            current.append(MeasureDeviceConnect.ldc.ld_current_reading())
-            voltage.append(MeasureDeviceConnect.ldc.ld_voltage_reading())
-            power.append(MeasureDeviceConnect.pm100.get_power())
-            J = np.array(current, dtype=float)
-            V = np.array(voltage, dtype=float)
-            L = np.array(power, dtype=float)
-            np.savetxt('data.txt', zip(J, V, L), fmt='%1.12e', header=' J [A] \t V \t L [w] ')
-            time.sleep(0.1)
-
-        MeasureDeviceConnect.ldc.off()
+            for i in range(0, len(set_current_array)):
+                MeasureDeviceConnect.ldc.ld_current_in_A_setpoint(str(set_current_array[i]))
+                current.append(MeasureDeviceConnect.ldc.ld_current_reading())
+                voltage.append(MeasureDeviceConnect.ldc.ld_voltage_reading())
+                power.append(MeasureDeviceConnect.pm100.get_power())
+                J = np.array(current, dtype=float)
+                V = np.array(voltage, dtype=float)
+                L = np.array(power, dtype=float)
+                np.savetxt('data.txt', zip(J, V, L), fmt='%1.12e', header=' J [A] \t V \t L [w] ')
+                time.sleep(0.1)
+        except Exception as err:
+                print(err)
+        finally:
+            MeasureDeviceConnect.ldc.off()
 
 
 class App(QMainWindow, MeasureDeviceConnect):
@@ -98,6 +103,10 @@ class App(QMainWindow, MeasureDeviceConnect):
         buton_to_save_data.clicked.connect(self.save_data)
         buton_to_save_data.move(1020, 100)
         buton_to_save_data.resize(140, 50)
+
+        label_with_current_wavelength = QLabel('Current wavelength: ' + str(self.CURRENT_WAVELENGTH ) + " nm", self)
+        label_with_current_wavelength.move(1020, 150)
+        label_with_current_wavelength.resize(200, 80)
 
         button_to_start_measure = QPushButton('Start', self)
         button_to_start_measure.move(1020, 10)
@@ -202,15 +211,15 @@ class PlotCanvas(FigureCanvas, BaseMeasureInfo):
                 current, voltage, power = np.loadtxt("data.txt", unpack=True, skiprows=1)
                 ax1.clear()
                 ax2.clear()
-                ax1.plot(current, power, 'bo')
-                ax2.plot(current, voltage, 'ro')
+                ax1.plot(current, power, 'ro')
+                ax2.plot(current, voltage, 'bo')
                 ax1.set_xlabel("J [A]")
                 ax1.set_ylabel('L [W]', color='r')
-                ax2.set_ylabel('U [V]', color='g')
+                ax2.set_ylabel('U [V]', color='b')
                 plt.grid(True)
             except Exception as msg_error:
                 self.OUT_MSG = self.OUT_MSG + "\n" + str(msg_error)
-        ani = animation.FuncAnimation(self.figure, animate, interval=200)
+        ani = animation.FuncAnimation(self.figure, animate, interval=100)
         self.draw()
 
 
